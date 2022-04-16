a8e3ccaa5da6c9d101a59785b897edbd01d9a7a2
Johannes Günther <103576477+johgyy@users.noreply.github.com>
2022-04-16 11:19:00 +0200

    added 3 jason objects

---
+++ script.js
@@ -0,0 +1,25 @@
+let questions = [{
+    "question": "Gallia in partes tres dividitur",
+    "answer_1": "Gallien wurde in 3 Teile geteilt",
+    "answer_2": "Gallien teilt sich in 3 Teile",
+    "answer_3": "Gallien wird in 3 Teile geteilt",
+    "answer_4": "In Gallien dividiert er durch 3",
+    "right_answer": 3
+},
+{
+    "question": "Rex militibus con suis in fines nostrae provinciae pervenit",
+    "answer_1": "Rex gelangte mit seinen Soldaten an die Grenzen von unserer Provinz",
+    "answer_2": "Der König gelangte mit seinem Militär an die Grenze unserer Provinz",
+    "answer_3": "Der König gelangte mit seinen Soldaten zu unserer Provinz",
+    "answer_4": "Der König gelangte mit seinen Soldaten an die Grenzen unserer Provinz",
+    "right_answer": 4
+},
+{
+    "question": "Iulia est puella tam pulchra quam rosae et lilia",
+    "answer_1": "Iulia ist ein Mädchen, so schön wie Rosen und Lilien",
+    "answer_2": "Iulia ist ein rosa und lila Mädchen",
+    "answer_3": "Iulia ist schöner als Rosen und Lilien",
+    "answer_4": "Iulia ist ein Mädchen, das gerne Rosen und Lilien mag",
+    "right_answer": 1
+}
+];
