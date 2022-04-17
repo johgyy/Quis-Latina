6879bb4e9b9c03a10711443a24e70997576a98d7
Johannes Günther <103576477+johgyy@users.noreply.github.com>
2022-04-17 12:06:36 +0200

    js angefangen 3fragen

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let questions = [{
     "question": "Gallia in partes tres dividitur",
     "answer_1": "Gallien wurde in 3 Teile geteilt",
-    "answer_2": "Gallien teilt sich in 3 Teile",
+    "answer_2": "In Gallien wird auf 3 Partys geteilt",
     "answer_3": "Gallien wird in 3 Teile geteilt",
-    "answer_4": "In Gallien dividiert er durch 3",
+    "answer_4": "In Gallien dividierte er durch 3",
     "right_answer": 3
 },
 {
@@ -23,3 +23,87 @@ let questions = [{
     "right_answer": 1
 }
 ];
+
+let currentquestion = 0;
+
+
+function init() {
+    document.getElementById('amountofquestions').innerHTML = questions.length;
+    showquestion();
+}
+
+function showquestion() {
+    if (currentquestion >= questions.length) {
+        document.getElementById('questionnumber').innerHTML = currentquestion;
+        document.getElementById('question-footer').innerHTML = `
+        <span>
+        <b id="questionnumber">${currentquestion}</b> von <b id="amountofquestions"5>${questions.length}</b> Fragen
+    </span>
+        <button onclick="showendscreen()" id="c" class="nextquestionbutton" disabled><a href="http://johannes-guenther.developerakademie.net/LATINA%20SIMPLEX/">Abschließen</a></button>
+        `;
+        resetanswerfields();
+        document.getElementById('answercontainer').innerHTML = `
+        <div class="endscreen">
+            
+        </div>
+        `;
+    } else {
+        let question = questions[currentquestion];  // die variable question ist das array questions an der stelle von der zahl von current question
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
+}
+
+function answer(selection) {
+
+    let question = questions[currentquestion];
+    // die variable question ist das array questions an der stelle von der zahl von current question
+    let selectedquestionnumber = selection.slice(-1);  // hiermit holt man sich die letzte stelle also buchstabe von dem wert
+    if (selectedquestionnumber == question['right_answer']) {
+        document.getElementById(selection).classList.remove('white');
+        document.getElementById(selection).classList.add('green');
+
+    } else {
+        document.getElementById(selection).classList.remove('white');
+        document.getElementById(selection).classList.add('red');
+        // richtige antwort grün machen
+        let idofrightanswer = `answer_${question['right_answer']}`; // hier bildet man jetzt sich die id, indem man answer plus den entsprechenden wert vom array questions an der stelle von currentquestion(oben schon eingebaut in der var) mit der eigenschaft "right answer"
+
+        document.getElementById(idofrightanswer).classList.remove('white');
+        document.getElementById(idofrightanswer).classList.add('green');
+    };
+    document.getElementById('nextquestionbutton').disabled = false;
+}
+
+function nextquestion() {
+    currentquestion++;
+    questioncounter();
+    document.getElementById('nextquestionbutton').disabled = true;
+    resetcolors();
+    showquestion();
+
+}
+
+function resetcolors() {
+    document.getElementById('answer_1').classList.remove('green', 'red');
+    document.getElementById('answer_2').classList.remove('green', 'red');
+    document.getElementById('answer_3').classList.remove('green', 'red');
+    document.getElementById('answer_4').classList.remove('green', 'red');
+}
+
+function questioncounter() {
+    document.getElementById('questionnumber').innerHTML = currentquestion + 1;
+
+}
+
+function resetanswerfields() {
+    document.getElementById('questiontext').innerHTML = 'Vielen Dank für deine Teilnahme!';
+    document.getElementById('answer_1').innerHTML = '';
+    document.getElementById('answer_2').innerHTML = '';
+    document.getElementById('answer_3').innerHTML = '';
+    document.getElementById('answer_4').innerHTML = '';
+    document.getElementById('questionstyle').innerHTML = '';
+}
