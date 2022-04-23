3a57aa78a2cce2bee3f5557321f40af45a2294d3
Johannes Günther <103576477+johgyy@users.noreply.github.com>
2022-04-23 19:18:04 +0200

    progress bar done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let questions = [{
     "question": "Gallia in partes tres dividitur",
     "answer_1": "Gallien wurde in 3 Teile geteilt",
-    "answer_2": "In Gallien wird auf 3 Partys geteilt",
+    "answer_2": "In Gallien wird auf 3 Partys ausgeteilt",
     "answer_3": "Gallien wird in 3 Teile geteilt",
     "answer_4": "In Gallien dividierte er durch 3",
     "right_answer": 3
@@ -21,10 +21,21 @@ let questions = [{
     "answer_3": "Iulia ist schöner als Rosen und Lilien",
     "answer_4": "Iulia ist ein Mädchen, das gerne Rosen und Lilien mag",
     "right_answer": 1
+},
+{
+    "question": "Caesar cum legionibus duobus ad Rhenum pervenit",
+    "answer_1": "Caesar kam mit seinen Legionen zum Rhein",
+    "answer_2": "Caesar und 2 Legionen kamen an den Rhein",
+    "answer_3": "Caesar gelangte mit 2 Legionen an den Rhein",
+    "answer_4": "Caesar schickte seine 2 Legionen zum Rhein",
+    "right_answer": 3
 }
 ];
 
+let correctandfalsanswers = []; //hierrein pusch ich bei einer richtigen antwort "true", bei falscher "false"
+
 let currentquestion = 0;
+let numberofrightanswers = 0;
 
 
 function init() {
@@ -32,18 +43,45 @@ function init() {
     showquestion();
 }
 
-function showquestion() {
-    if (currentquestion == questions.length - 1) {
-        document.getElementById('nextquestionbutton').innerHTML = 'Abschließen'
-    } else {
-        if (currentquestion == questions.length) {
+function showquestion() { // show endscreen
+    if (currentquestion == questions.length) {
+        // fragen ausblenden
+        document.getElementById('question-footer').classList.add('d-none');
+        document.getElementById('answercontainer').classList.add('d-none');
+        document.getElementById('finalscore').classList.remove('d-none');
 
-            document.getElementById('question-footer').classList.add('d-none');
-            document.getElementById('answercontainer').classList.add('d-none');
-            document.getElementById('finalscore').classList.remove('d-none')
-            resetanswerfields();
 
-        } else {
+
+        // show the final screen
+        document.getElementById('finalscore').innerHTML += /*html*/ `
+        <span>
+            <p>Du hast <b>${numberofrightanswers}</b> von <b>${questions.length}</b> Fragen richtig!</p>
+        </span>
+
+        <button onclick="restart()" id="" class="nextquestionbutton">Quiz wiederholen</button>
+        
+        <button onclick="showendscreen()" id="" class="nextquestionbutton"><a
+                        href="http://johannes-guenther.developerakademie.net/LATINA%20SIMPLEX/">Zurück zur
+                        Startseite</a></button>
+        `;
+        resetanswerfields();
+
+    } else { //show questions
+        if (currentquestion == questions.length - 1) {
+            document.getElementById('nextquestionbutton').innerHTML = 'Abschließen';
+        }
+            let percent = (currentquestion + 1) / questions.length;
+            percent = Math.round(percent * 100);
+        
+
+        document.getElementById('progress').innerHTML = `
+        <div id="progress-bar" class="progress-bar" role="progressbar" style="width: ${percent}%;" aria-valuenow="${percent}" aria-valuemin="0" aria-valuemax="100">${percent} %</div>     
+        `;
+        if (percent == 100) {
+            document.getElementById('progress-bar').classList.add('progressbarborderstyleleft');
+        }
+
+
             let question = questions[currentquestion];  // die variable question ist das array questions an der stelle von der zahl von current question
             document.getElementById('questiontext').innerHTML = question['question'];
             document.getElementById('answer_1').innerHTML = question['answer_1'];
@@ -52,55 +90,60 @@ function showquestion() {
             document.getElementById('answer_4').innerHTML = question['answer_4'];
         }
     }
-}
 
-function answer(selection) { // selection ist jetzt der name der id, weil wir das in der html datei so eingegeben haben
 
-    let question = questions[currentquestion];
-    // die variable question ist das array questions an der stelle von der zahl von current question
-    let selectedquestionnumber = selection.slice(-1);  // hiermit holt man sich die letzte stelle also buchstabe von dem wert (x) ist die länge eder zeichen, die geholt wird.
-    if (selectedquestionnumber == question['right_answer']) {
-        document.getElementById(selection).classList.remove('white');
-        document.getElementById(selection).classList.add('green');
+    function answer(selection) { // selection ist jetzt der name der id, weil wir das in der html datei so eingegeben haben
 
-    } else {
-        document.getElementById(selection).classList.remove('white');
-        document.getElementById(selection).classList.add('red');
-        // richtige antwort grün machen
-        let idofrightanswer = `answer_${question['right_answer']}`; // hier bildet man jetzt sich die id, indem man answer plus den entsprechenden wert vom array questions an der stelle von currentquestion(oben schon eingebaut in der var) mit der eigenschaft "right answer"
+        let question = questions[currentquestion];
+        // die variable question ist das array questions an der stelle von der zahl von current question
+        let selectedquestionnumber = selection.slice(-1);  // hiermit holt man sich die letzte stelle also buchstabe von dem wert (x) ist die länge eder zeichen, die geholt wird.
+        if (selectedquestionnumber == question['right_answer']) {
+            document.getElementById(selection).classList.remove('white');
+            document.getElementById(selection).classList.add('green');
+            numberofrightanswers++;
 
-        document.getElementById(idofrightanswer).classList.remove('white');
-        document.getElementById(idofrightanswer).classList.add('green');
-    };
-    document.getElementById('nextquestionbutton').disabled = false;
-}
+        } else {
+            document.getElementById(selection).classList.remove('white');
+            document.getElementById(selection).classList.add('red');
+            // richtige antwort grün machen
+            let idofrightanswer = `answer_${question['right_answer']}`; // hier bildet man jetzt sich die id, indem man answer plus den entsprechenden wert vom array questions an der stelle von currentquestion(oben schon eingebaut in der var) mit der eigenschaft "right answer"
 
-function nextquestion() {
-    currentquestion++;
-    questioncounter();
-    document.getElementById('nextquestionbutton').disabled = true;
-    resetcolors();
-    showquestion();
+            document.getElementById(idofrightanswer).classList.remove('white');
+            document.getElementById(idofrightanswer).classList.add('green');
+        };
+        document.getElementById('nextquestionbutton').disabled = false;
+    }
 
-}
+    function nextquestion() {
+        currentquestion++;
+        questioncounter();
+        document.getElementById('nextquestionbutton').disabled = true;
+        resetcolors();
+        showquestion();
 
-function resetcolors() {
-    document.getElementById('answer_1').classList.remove('green', 'red');
-    document.getElementById('answer_2').classList.remove('green', 'red');
-    document.getElementById('answer_3').classList.remove('green', 'red');
-    document.getElementById('answer_4').classList.remove('green', 'red');
-}
+    }
 
-function questioncounter() {
-    document.getElementById('questionnumber').innerHTML = currentquestion + 1;
+    function resetcolors() {
+        document.getElementById('answer_1').classList.remove('green', 'red');
+        document.getElementById('answer_2').classList.remove('green', 'red');
+        document.getElementById('answer_3').classList.remove('green', 'red');
+        document.getElementById('answer_4').classList.remove('green', 'red');
+    }
 
-}
+    function questioncounter() {
+        document.getElementById('questionnumber').innerHTML = currentquestion + 1;
 
-function resetanswerfields() {
-    document.getElementById('questiontext').innerHTML = 'Vielen Dank für deine Teilnahme!';
-    document.getElementById('answer_1').innerHTML = '';
-    document.getElementById('answer_2').innerHTML = '';
-    document.getElementById('answer_3').innerHTML = '';
-    document.getElementById('answer_4').innerHTML = '';
-    document.getElementById('questionstyle').innerHTML = '';
-}
+    }
+
+    function resetanswerfields() {
+        document.getElementById('questiontext').innerHTML = 'Vielen Dank für deine Teilnahme!';
+        document.getElementById('answer_1').innerHTML = '';
+        document.getElementById('answer_2').innerHTML = '';
+        document.getElementById('answer_3').innerHTML = '';
+        document.getElementById('answer_4').innerHTML = '';
+        document.getElementById('questionstyle').innerHTML = '';
+    }
+
+    function restart() {
+        
+    }
